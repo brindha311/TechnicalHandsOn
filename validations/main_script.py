a432e9b98062a8883c1dcf8ccb4de9c66f9186e8
brindha311 <154029167+brindha311@users.noreply.github.com>
2023-12-17 02:15:50 +0100

    primary version of duplicate check handson

---
+++ validations/main_script.py
@@ -0,0 +1,40 @@
+"""
+Script Name: main_script.py
+Description: script uses the DataValidation utility to check for duplicates in specified columns.
+Author: B S
+Date : 16.12.2023
+
+"""
+import pandas as pd
+from utilities.data_validation import DataValidation
+
+
+def main():
+    # Test the class with the provided dataset
+    df_1 = pd.DataFrame(
+        data=[
+            ['A', 'a', 'x', 1],
+            ['A', 'b', 'x', 1],
+            ['A', 'c', 'x', 1],
+            ['B', 'a', 'x', 1],
+            ['B', 'b', 'x', 1],
+            ['B', 'c', 'x', 1],
+            ['A', 'a', 'y', 1],
+        ],
+        columns=['col_1', 'col_2', 'col_3', 'col_4']
+    )
+    # Initialize DataValidation with the dataframe
+    duplicate_checker = DataValidation(df_1)
+    # Test duplicates for different combination of columns
+    columns_list = [['col_1'], ['col_1', 'col_2'], ['col_1', 'col_2', 'col_3']]
+
+    # loop to print duplicates for each combination of columns
+    for col in columns_list:
+        result = duplicate_checker.check_duplicates(col)
+        print(result)
+        # better visibility
+        print("-" * 50)
+
+
+if __name__ == "__main__":
+    main()
